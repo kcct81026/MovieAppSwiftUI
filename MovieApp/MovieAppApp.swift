7120c5f17613139c64d7073e8a386a35c14607fb
Khin Cho <ms.khincho90@gmail.com>
2023-10-04 00:35:48 +0630

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by AyeSuNaing on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
